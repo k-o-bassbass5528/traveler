f88b36f97896cc696c58ba4a06ace799638f03ba
k-o-bassbass5528 <198025965+k-o-bassbass5528@users.noreply.github.com>
2025-03-01 23:29:17 +0900

    [add]DM機能（未完成）

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   resources :users, only:[:index, :show, :edit, :update] do
     member do
-      get :follows, :followers
+      get :follows, :followers, :talkroom
     end
     resource :relationships, only: [:create, :destroy]
   end
