880d0006f8c185891d7f4886c02b22feb21c44c9
k-o-bassbass5528 <198025965+k-o-bassbass5528@users.noreply.github.com>
2025-03-04 22:09:54 +0900

    [add]DM機能（途中）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,29 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+
   def index
     @users = User.page(params[:page]).per(5).reverse_order
   end
 
   def show
     @user = User.find(params[:id])
+    @current_entry = Entry.where(user_id: current_user.id)
+    @another_entry = Entry.where(user_id: @user.id)
+    unless @user.id == current_user.id
+      @current_entry.each do |current|
+        @another_entry each do |another|
+          if current.room_id == another.room_id
+            @is_room = true
+            @room_id = current.room_id
+          end
+        end
+      end
+      unless @is_room
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
     @posts = @user.posts.page(params[:page]).per(8).reverse_order
     @following_users = @user.following_user
     @follower_users = @user.follower_user
@@ -28,10 +47,6 @@ class UsersController < ApplicationController
   def followers
     user = User.find(params[:id])
     @users = user.follower_user.page(params[:page]).per(3).reverse_order
-  end
-
-  def talkroom
-    @user = User.find(params[:id])
   end
 
   private
