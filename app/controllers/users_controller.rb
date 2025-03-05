a3a0b9fc923bdcbdd123d255f7dd732201cc678c
k-o-bassbass5528 <198025965+k-o-bassbass5528@users.noreply.github.com>
2025-03-05 22:38:56 +0900

    [add]DM機能（途中）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
-
+  before_action :authenticate_user!, only: [:show]
 
   def index
     @users = User.page(params[:page]).per(5).reverse_order
@@ -8,18 +7,20 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @current_entry = Entry.where(user_id: current_user.id)
-    @another_entry = Entry.where(user_id: @user.id)
-    unless @user.id == current_user.id
-      @current_entry.each do |current|
-        @another_entry each do |another|
-          if current.room_id == another.room_id
-            @is_room = true
-            @room_id = current.room_id
+    @currentUserEntry = Entry.where(user_id: current_user.id)
+    @userEntry = Entry.where(user_id: @user.id)
+    if @user.id == current_user.id
+    else
+      @currentUserEntry.each do |cu|
+        @userEntry.each do |u|
+          if cu.room_id == u.room_id then
+            @isRoom = true
+            @roomId = cu.room_id
           end
         end
       end
-      unless @is_room
+      if @isRoom
+      else
         @room = Room.new
         @entry = Entry.new
       end
@@ -53,4 +54,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :profile, :profile_image)
   end
+
 end
